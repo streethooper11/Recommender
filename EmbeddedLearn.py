cfaebc512ebca2707eb19c92dbc6757d041da15d
Daniel Sehwa Kim <67356128+streethooper11@users.noreply.github.com>
2023-03-05 21:14:09 -0700

    Split up into smaller functions

--- EmbeddedLearn.py
+++ EmbeddedLearn.py
@@ -4,32 +4,15 @@ import torch
 from transformers import BertTokenizer, BertModel
 
 
-def embedSentence(trained_model, tokenizer_name, sentence):
+def getTokenEmbeddings(trained_model, indexed_tokens, segments_ids):
     """
-    Performs word embedding with the given BERT model, tokenizer name,
-    and a sentence.
+    Obtains token embeddings from the indexed tokens and segment IDs
 
-    :param trained_model: The trained BERT model
-    :param tokenizer_name: The name of the tokenizer
-    :param sentence: Given sentence to create a vector for
-    :return: A list of vectors that represents the words in the sentence
-
+    :param trained_model: Trained BERT model
+    :param indexed_tokens: Indexed tokens
+    :param segments_ids: List of segment IDs
+    :return:The embeddings in the sentence
     """
-
-    # Load pre-trained model tokenizer with a given name
-    tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
-
-    # Add the special tokens.
-    marked_text = "[CLS] " + sentence + " [SEP]"
-
-    # Split the sentence into tokens.
-    tokenized_text = tokenizer.tokenize(marked_text)
-
-    # Map the token strings to their vocabulary indices.
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-
-    # Mark each of the tokens as belonging to sentence "1", as each element is 1 sentence.
-    segments_ids = [1] * len(tokenized_text)
 
     # Convert inputs to PyTorch tensors
     tokens_tensor = torch.tensor([indexed_tokens])
@@ -50,22 +33,66 @@ def embedSentence(trained_model, tokenizer_name, sentence):
     # Concatenate the tensors for all layers. We use `stack` here to
     # create a new dimension in the tensor.
     token_embeddings = torch.stack(hidden_states, dim=0)
+    # Remove dimension 1, the "batches".
+    token_embeddings = torch.squeeze(token_embeddings, dim=1)
 
-    # For each token in the sentence...
-    for token in token_embeddings:
-        # Remove dimension 1, the "batches".
-        token_embeddings = torch.squeeze(token_embeddings, dim=1)
+    return token_embeddings
+
+
+def indexAndSetSegmentIds(tokenizer_name: str, sentence: str):
+    """
+    Maps the token strings to vocabulary indices
+    and mark the tokens
+
+    :param tokenizer_name: The name of the tokenizer
+    :param sentence: A given sentence
+    :return: Indexed tokens and segment IDs
+    """
+
+    # Load pre-trained model tokenizer with a given name
+    tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
+
+    # Add the special tokens.
+    marked_text = "[CLS] " + sentence + " [SEP]"
+
+    # Split the sentence into tokens.
+    tokenized_text = tokenizer.tokenize(marked_text)
+
+    # Map the token strings to their vocabulary indices.
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+    # Mark each of the tokens as belonging to sentence "1", as each element is 1 sentence.
+    segments_ids = [1] * len(tokenized_text)
+
+    return indexed_tokens, segments_ids
+
+
+def embedSentence(trained_model, tokenizer_name: str, sentence: str):
+    """
+    Performs word embedding with the given BERT model, tokenizer name,
+    and a sentence.
+
+    :param trained_model: The trained BERT model
+    :param tokenizer_name: The name of the tokenizer
+    :param sentence: Given sentence to create a vector for
+    :return: A list of vectors that represents the words in the sentence
+    """
+
+    indexed_tokens, segments_ids = indexAndSetSegmentIds(tokenizer_name, sentence)
+    token_embeddings = getTokenEmbeddings(trained_model, indexed_tokens, segments_ids)
 
     # Stores the token vectors
     token_vecs_cat = []
 
-    # Concatenate the vectors (that is, append them together) from the last
-    # four layers.
-    # Each layer vector is 768 values, so `cat_vec` is length 3,072.
-    cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
+    # For each token in the sentence...
+    for token in token_embeddings:
+        # Concatenate the vectors (that is, append them together) from the last
+        # four layers.
+        # Each layer vector is 768 values, so `cat_vec` is length 3,072.
+        cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
 
-    # Use `cat_vec` to represent `token`.
-    token_vecs_cat.append(cat_vec)
+        # Use `cat_vec` to represent `token`.
+        token_vecs_cat.append(cat_vec)
 
     return token_vecs_cat
 
