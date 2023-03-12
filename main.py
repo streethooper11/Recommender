386cf1b6c057bb2bc901759bd2b67d3990eeca65
Daniel Sehwa Kim <67356128+streethooper11@users.noreply.github.com>
2023-03-11 17:39:21 -0700

    Errorfix

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 import EmbeddedLearn
 import Clustering
 
-csvLoc = sys.argv[1]
+# csvLoc = sys.argv[1]
+csvLoc = 'output.csv'
 
 all_vectors = EmbeddedLearn.embedWords(csvLoc, 'bert-base-uncased')
 clusters = Clustering.dbscanClustering(all_vectors)
