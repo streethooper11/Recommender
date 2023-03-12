9ed8a70a3356c9814188efc2ef938e06983d562f
Daniel Sehwa Kim <67356128+streethooper11@users.noreply.github.com>
2023-03-11 19:17:52 -0700

    change csvLoc

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import EmbeddedLearn
 import Clustering
 
 # csvLoc = sys.argv[1]
-csvLoc = 'output_backup.csv'
+csvLoc = 'output.csv'
 
 all_vectors = EmbeddedLearn.embedWords(csvLoc, 'bert-base-uncased')
 data_with_clusters = Clustering.dbscanClustering(all_vectors)
