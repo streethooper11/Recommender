31c1965cedc96380584cf9e1e520b619089b901d
Sammy Tran <sammy.tran1@ucalgary.ca>
2023-03-30 09:38:39 -0600

    got query terms and clsuter info

--- extractTerms.py
+++ extractTerms.py
@@ -1,5 +1,6 @@
 #https://www.freecodecamp.org/news/how-to-process-textual-data-using-tf-idf-in-python-cd2bbc0a94a3/
 #https://stackoverflow.com/questions/60642043/how-fit-transform-transform-and-tfidfvectorizer-works
+#https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
 
 import pandas as pd
 import numpy as np
@@ -24,6 +25,7 @@ def extractTerms(k, df):
 
     :param k the number of terms to get
     :param df the dataframe of subwords and cluster values
+    return list of tuples (term, cluster value)
     """
     terms = []
     only_words = df["subwords"].to_list()
@@ -32,5 +34,16 @@ def extractTerms(k, df):
     response = vectorizer.fit_transform(only_words)
     feature_names = vectorizer.get_feature_names_out()
 
-    return terms
+    #get tfidf scores for input document
+    tfidf_scores = {}
+    for col in response.nonzero()[1]:
+        tfidf_scores[feature_names[col]] = response[0, col]
     
+    sorted_by_scores = sorted(tfidf_scores.items(), key=lambda x:x[1], reverse=True)
+    for i in range(k):
+        term = sorted_by_scores[i]
+        for j in range(df.shape[0]):
+            if df.iloc[j]["subwords"] == term[0]:
+                terms.append((term[0],df.iloc[j]["cluster"]))
+
+    return terms
