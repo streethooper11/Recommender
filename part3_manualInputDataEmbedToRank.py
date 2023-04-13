c2e1b3f34b212e040c9c2c7f28b14f7b72d630ab
Daniel Sehwa Kim <67356128+streethooper11@users.noreply.github.com>
2023-04-12 20:46:21 -0600

    Final push

--- part3_manualInputDataEmbedToRank.py
+++ part3_manualInputDataEmbedToRank.py
@@ -120,8 +120,8 @@ if __name__ == "__main__":
     # Return the total number of correct predictions
     # Option to run DBSCAN
     numMatch = clusterToRankGen(input_actors, up_input_subwords, up_input_vectors, eps=12, min_samples=6,
-                                query_terms=10, similarity_w=3,
-                                popularity_w=0, rating_w=1, topNum=7)
+                                query_terms=10, similarity_w=7,
+                                popularity_w=3, rating_w=3, topNum=7)
     # Option to run K-means clustering
     #numMatch = clusterToRankGen(input_actors, up_input_subwords, up_input_vectors, n_clusters=40,
     #                            query_terms=10, similarity_w=3,
